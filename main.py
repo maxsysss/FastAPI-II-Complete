0e803b25d4fc11cdfa35fa4c971d83674f5789a0
Maximiliano Rodrigues <maximilianorodrigues83@hotmail.com>
2026-04-28 13:15:19 -0300

    exceptions

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
 from typing import Optional
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
 from router import blog_get
 from router import blog_post
 from router import user
 from router import article
 from db.database import engine
 from db import models
+from exceptions import StoryException
+from fastapi.responses import JSONResponse
 
 
 app = FastAPI()
@@ -17,5 +19,13 @@ app.include_router(blog_post.router)
 @app.get('/hello')
 def index():
   return {'message': 'Hello world!'}
+
+
+@app.exception_handler(StoryException)
+def story_exception_handler(request: Request, exc: StoryException):
+  return JSONResponse(
+    status_code=418,
+    content={"detail": exc.name}
+  )
 
 models.Base.metadata.create_all(engine)
