7c2a0c3180ffe41615e657351201e44ba4b5e7e8
Maximiliano Rodrigues <maximilianorodrigues83@hotmail.com>
2026-04-29 09:29:22 -0300

    responses plain text, xml, html, files, streaming

--- router/product.py
+++ router/product.py
@@ -14,11 +14,28 @@ def get_all_products():
     data = " ".join(products)
     return Response(content=data, media_type="text/plain")
 
-@router.get("/{id}")
+@router.get("/{id}", responses={
+    200: {
+        "content": {
+            "text/html": {
+                "example": "<div>Product</div>"
+            }
+        },
+        "description": "Returns the HTML for an object"
+    },
+    404: {
+        "content": {
+            "text/plain": {
+                "example": "Produtc not available"
+            }
+        },
+        "description": "A cleartext error message"        
+    }
+})
 def get_product(id: int):
     if id > len(products):
         out = "Product not available"
-        return PlainTextResponse(content=out, media_type="text/plain")
+        return PlainTextResponse(status_code=404, content=out, media_type="text/plain")
     else:
         product = products[id]
         out = f"""
